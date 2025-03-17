e4f323dcc0ebbe03c97a0cb03cd739f3bb0f74ca
Born24 <106234496+Born24@users.noreply.github.com>
2025-03-17 16:18:30 +0300

    исправил

--- src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
@@ -3,15 +3,14 @@ package ru.kata.spring.boot_security.demo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import ru.kata.spring.boot_security.demo.model.User;
 import ru.kata.spring.boot_security.demo.service.UserService;
 
 import java.util.List;
 
 @Controller
-@RequestMapping("/admin")
+@RequestMapping("/")
 public class AdminController {
 
     private final UserService userService;
@@ -21,12 +20,58 @@ public class AdminController {
         this.userService = userService;
     }
 
-    @GetMapping
+
+    @GetMapping("/findById")
+    public String findById(@RequestParam("id") Long id, Model model) {
+        User user = userService.findById(id);
+        model.addAttribute("user", user);
+
+        return "findById";
+    }
+
+    @PostMapping("/addUser")
+    public String saveUser(@ModelAttribute("user") User user) {
+        userService.add(user);
+        return "redirect:/admin";
+    }
+
+    @GetMapping("/update/{id}")
+    public String editUserForm(@PathVariable("id") Long id, Model model) {
+        User user = userService.findById(id);
+        model.addAttribute("user", user);
+
+        return "edit";
+
+    }
+
+    @PostMapping("/update/{id}")
+    public String updateUser(@ModelAttribute("user") User user, @PathVariable("id") Long id) {
+        user.setId(id);
+        userService.update(user);
+
+        return "redirect:/admin";
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public String deleteUser(@PathVariable("id") Long id) {
+        userService.delete(id);
+
+        return "redirect:/admin";
+    }
+
+    @GetMapping("/users/{id}")
+    public String viewUser(@PathVariable Long id, Model model) {
+        User user = userService.findById(id);
+        model.addAttribute("user", user);
+
+        return "show";
+    }
+
+    @GetMapping("/admin")
     public String adminPage(Model model) {
         List<User> users = userService.findAll();
         model.addAttribute("users", users);
-        return "admin"; // Возвращает шаблон 'admin'
+        return "admin";
     }
-
 
 }
--- src/main/java/ru/kata/spring/boot_security/demo/controller/UsersController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/UsersController.java
@@ -20,58 +20,6 @@ public class UsersController {
         this.userService = userService;
     }
 
-    @GetMapping
-    public String listUsers(Model model) {
-        model.addAttribute("users", userService.findAll());
-        return "users";
-    }
-
-    @GetMapping("/findById")
-    public String findById(@RequestParam("id") Long id, Model model) {
-        User user = userService.findById(id);
-        model.addAttribute("user", user);
-
-        return "findById";
-    }
-
-    @PostMapping("/addUser")
-    public String saveUser(@ModelAttribute("user") User user) {
-        userService.add(user);
-        return "redirect:/admin";
-    }
-
-    @GetMapping("/update/{id}")
-    public String editUserForm(@PathVariable("id") Long id, Model model) {
-        User user = userService.findById(id);
-        model.addAttribute("user", user);
-
-        return "edit";
-
-    }
-
-    @PostMapping("/update/{id}")
-    public String updateUser(@ModelAttribute("user") User user, @PathVariable("id") Long id) {
-        user.setId(id);
-        userService.update(user);
-
-        return "redirect:/admin";
-    }
-
-    @DeleteMapping("/delete/{id}")
-    public String deleteUser(@PathVariable("id") Long id) {
-        userService.delete(id);
-
-        return "redirect:/admin";
-    }
-
-    @GetMapping("/users/{id}")
-    public String viewUser(@PathVariable Long id, Model model) {
-        User user = userService.findById(id);
-        model.addAttribute("user", user);
-
-        return "show";
-    }
-
     @GetMapping("/user")
     public String userProfile(Principal principal, Model model) {
         String username = principal.getName();
